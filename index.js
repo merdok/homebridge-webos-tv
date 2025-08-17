9f52ba4b2578e6a7e2a613ffeda1a7f37a337b8d
passibe15 <46612352+passibe15@users.noreply.github.com>
2025-08-17 04:46:48 +0200

    Add SHOW_INPUT_PICKER remote command

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let Service, Characteristic, Homebridge, Accessory, HapStatusError, HAPStatus, H
 
 const PLUGIN_NAME = 'homebridge-webos-tv';
 const PLATFORM_NAME = 'webostv';
-const PLUGIN_VERSION = '2.4.6';
+const PLUGIN_VERSION = '2.4.7';
 
 // General constants
 const NOT_EXISTING_INPUT = 999999;
